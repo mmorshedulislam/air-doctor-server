27fe056e2057ccfdbb005b0b292884259d37513b
MORSHED-HOME <morshed952640@gmail.com>
2022-11-08 20:47:10 +0600

    limit 3 service done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,24 @@ async function run() {
       res.send({
         count,
         services,
+      });
+    });
+
+    // find limit 3 service
+    app.get("/service3", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const service = await cursor.limit(3).toArray();
+      res.send(service);
+    });
+    // find a service by id
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send({
+        status: true,
+        service,
       });
     });
 
